cf1f8a314598004da40e6ff39b0fbaf784731464
Jcchen <wjcrhr@qq.com>
2022-08-18 17:11:40 +0800

    native call flutter page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_boost/flutter_boost.dart';
 
+import 'flutter_page.dart';
+
 void main() {
   ///这里的CustomFlutterBinding调用务必不可缺少，用于控制Boost状态的resume和pause
   CustomFlutterBinding();
@@ -9,7 +11,8 @@ void main() {
 }
 
 ///创建一个自定义的Binding，继承和with的关系如下，里面什么都不用写
-class CustomFlutterBinding extends WidgetsFlutterBinding with BoostFlutterBinding {}
+class CustomFlutterBinding extends WidgetsFlutterBinding
+    with BoostFlutterBinding {}
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -22,6 +25,7 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return FlutterBoostApp(
+      initialRoute: "mainPage",
       routeFactory,
       appBuilder: appBuilder,
     );
@@ -29,6 +33,8 @@ class MyAppState extends State<MyApp> {
 
   Route<dynamic>? routeFactory(RouteSettings settings, String? uniqueId) {
     FlutterBoostRouteFactory? func = routerMap[settings.name];
+    debugPrint("Log_fangpao name =${settings.name}---,func=$func");
+
     if (func == null) {
       return null;
     }
@@ -48,18 +54,26 @@ class MyAppState extends State<MyApp> {
   //注册的路由表
   Map<String, FlutterBoostRouteFactory> routerMap = {
     'mainPage': (settings, id) {
-      return CupertinoPageRoute(
-          settings: settings,
-          builder: (_) {
-            Map<String, Object> map = settings.arguments as Map<String, Object>;
-            String data = map['data'] as String;
-            return SimplePage(
-              data: data,
-            );
-          });
+      return PageRouteBuilder<dynamic>(
+        settings: settings,
+        pageBuilder: (_, __, ___) => FlutterRouteWidget(
+          params: settings.arguments as Map<dynamic, dynamic>?,
+          uniqueId: "uniqueId",
+        ),
+      );
+      // return CupertinoPageRoute(
+      //     settings: settings,
+      //     builder: (_) {
+      //       Map<String, Object> map = settings.arguments as Map<String, Object>;
+      //       String data = map['data'] as String;
+      //       return MainPage(
+      //         data: data,
+      //       );
+      //     });
     },
   };
 }
+
 
 class MainPage extends StatelessWidget {
   const MainPage({required Object data});
@@ -78,10 +92,10 @@ class SimplePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-        child: Text(
-          'Hello, world!',
-          textDirection: TextDirection.ltr,
-        ),
-      );
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+      ),
+    );
   }
 }
